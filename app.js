b700c5da097570bdba4e47729c5167a3eaae3e5f
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-04-26 13:12:06 -0500

    adds comments and console.log to tell the user when the csv for the students plan is created

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// Fast-csv is a easy to use NPM package for parsing and creating .csv files
 const csv = require('fast-csv');
 const domainOrderRaw = [];
 const domainOrderCondensed = [];
@@ -7,10 +8,14 @@ const studentTestsCondensed = [];
 const studentPlans = [];
 
 
+// Function to start execution of the application
 run();
 
 
+// ==================== FUNCTIONS ==========================================================
 
+// Function to parse the domain_order and student_tests csv files then call the
+// createStudentPlans functions upon completion
 function run() {
     const domainOrderCSV = fs.createReadStream("./csv/domain_order.csv");
 
@@ -21,7 +26,6 @@ function run() {
         })
         .on("end", function () {
             console.log("domain_order.csv read successful");
-            console.log(domainOrderRaw);
             const studentTestCSV = fs.createReadStream("./csv/student_tests.csv");
 
             csv
@@ -30,24 +34,25 @@ function run() {
                     studentTests.push(data);
                 })
                 .on("end", function () {
-                    console.log("domain_order.csv read successful");
-                    console.log(studentTests);
+                    console.log("student_tests.csv read successful");
                     createStudentPlans();
                 });
         });
 };
 
+
+// Function to create the student plans from the parsed csv data in the run function
 function createStudentPlans() {
-
-
+    // Loops thorugh each row in the domain_order csv and appends the subject to the current
+    // grade level to get the data in GradeLevel.Subject format
     for (var i = 0; i < domainOrderRaw.length; i++) {
         for (var j = 1; j < domainOrderRaw[i].length; j++) {
             domainOrderCondensed.push(domainOrderRaw[i][0] + "." + domainOrderRaw[i][j]);
         };
     };
-    console.log(domainOrderCondensed);
 
-
+    // Creates a new array of arrays from the student_tests csv so that the new format of
+    // data is GradeLevel.Subject to match the domainOrderCondensed array
     for (var i = 1; i < studentTests.length; i++) {
         var newStudent = [];
         studentPlans.push([studentTests[i][0]]);
@@ -58,9 +63,9 @@ function createStudentPlans() {
 
         studentTestsCondensed.push(newStudent);
     };
-    console.log(studentTestsCondensed);
 
-
+    // Checks to see if the current index value for the plan sequence (domainOrderCondensed)
+    // exists in a students tested level, if it does it pushs that value into that students array
     for (var i = 0; i < domainOrderCondensed.length; i++) {
         for (var j = 0; j < studentTestsCondensed.length; j++) {
             if (studentTestsCondensed[j].includes(domainOrderCondensed[i])) {
@@ -68,13 +73,19 @@ function createStudentPlans() {
             }
         };
     };
-    console.log(studentPlans);
-    createStudentPlanCSV();
-}
 
+
+    createStudentPlanCSV();
+};
+
+// Function to create the student learning plans as a .csv file stored as
+// student_plans.csv under the ./csv/ directory
 function createStudentPlanCSV() {
     var ws = fs.createWriteStream("./csv/student_plans.csv");
     csv
         .write(studentPlans, { headers: true })
-        .pipe(ws);
-}
+        .pipe(ws)
+        .on("finish", function () {
+            console.log("\n" + "student_plans.csv created successfully!" + "\n");
+        });
+};
