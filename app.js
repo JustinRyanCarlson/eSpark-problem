2485b24202d50e639741adf7f5e5d91ad616b3ff
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-04-25 21:02:17 -0500

    adds the generated student_plans.csv to the gitignore, adds method to solve problem to app.js, adds function to write the new student_plan.csv file to the ./csv folder

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
 const csv = require('fast-csv');
-const domainOrder = [];
+const domainOrderRaw = [];
+const domainOrderCondensed = [];
 const studentTests = [];
+const studentTestsCondensed = [];
+const studentPlans = [];
 
 
-readCSVs();
+run();
 
 
-function readCSVs() {
+
+function run() {
     const domainOrderCSV = fs.createReadStream("./csv/domain_order.csv");
 
     csv
         .fromStream(domainOrderCSV)
         .on("data", function (data) {
-            domainOrder.push(data);
+            domainOrderRaw.push(data);
         })
         .on("end", function () {
             console.log("domain_order.csv read successful");
-            console.log(domainOrder);
+            console.log(domainOrderRaw);
             const studentTestCSV = fs.createReadStream("./csv/student_tests.csv");
 
             csv
@@ -28,6 +32,49 @@ function readCSVs() {
                 .on("end", function () {
                     console.log("domain_order.csv read successful");
                     console.log(studentTests);
+                    createStudentPlans();
                 });
         });
+};
+
+function createStudentPlans() {
+
+
+    for (var i = 0; i < domainOrderRaw.length; i++) {
+        for (var j = 1; j < domainOrderRaw[i].length; j++) {
+            domainOrderCondensed.push(domainOrderRaw[i][0] + "." + domainOrderRaw[i][j]);
+        };
+    };
+    console.log(domainOrderCondensed);
+
+
+    for (var i = 1; i < studentTests.length; i++) {
+        var newStudent = [];
+        studentPlans.push([studentTests[i][0]]);
+
+        for (var j = 1; j < studentTests[i].length; j++) {
+            newStudent.push(studentTests[i][j] + "." + studentTests[0][j]);
+        };
+
+        studentTestsCondensed.push(newStudent);
+    };
+    console.log(studentTestsCondensed);
+
+
+    for (var i = 0; i < domainOrderCondensed.length; i++) {
+        for (var j = 0; j < studentTestsCondensed.length; j++) {
+            if (studentTestsCondensed[j].includes(domainOrderCondensed[i])) {
+                studentPlans[j].push(domainOrderCondensed[i]);
+            }
+        };
+    };
+    console.log(studentPlans);
+    createStudentPlanCSV();
+}
+
+function createStudentPlanCSV() {
+    var ws = fs.createWriteStream("./csv/student_plans.csv");
+    csv
+        .write(studentPlans, { headers: true })
+        .pipe(ws);
 }
